b51337747177ed12a703c6205f7c6db277e5c76b
Maciej Duda <maciej.duda1@gazeta.pl>
2018-02-06 12:45:41 +0100

    1

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const express = require('express');
+const path = require('path');
+
 const app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
 app.set('view engine', 'pug');
-app.set('views', './views');
+app.set('views', path.join(__dirname + 'views'));
 
 app.get( '/', function(req, res){
     res.render('home-site'); 
@@ -15,7 +17,7 @@ app.get( '/', function(req, res){
 app.listen(app.get('port'), function() {
     console.log('Node app is running on port', app.get('port'));
   });
-  
+
 /*
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://mdtest:wow123@ds225308.mlab.com:25308/firstdatabasetest', {
